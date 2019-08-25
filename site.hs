4fffcacd940c0cc3fbafd9fac30fb6856bda9485
Jonathan Dowland <jon@dow.land>
2019-08-25 21:52:56 +0100

    implement tags
    following the guide at
    https://javran.github.io/posts/2014-03-01-add-tags-to-your-hakyll-blog.html

--- site.hs
+++ site.hs
@@ -13,6 +13,24 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
+    -- tags stuff thanks to
+    -- https://javran.github.io/posts/2014-03-01-add-tags-to-your-hakyll-blog.html
+    tags <- buildTags ("posts/*" .||. "contact.markdown") (fromCapture "tags/*.html")
+
+    tagsRules tags $ \tag pattern -> do
+        let title = "Posts tagged \"" ++ tag ++ "\""
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll pattern
+            let ctx = constField "title" title
+                      `mappend` listField "posts" postCtx (return posts)
+                      `mappend` defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/tag.html" ctx
+                >>= loadAndApplyTemplate "templates/default.html" ctx
+                >>= relativizeUrls
+
     match "posts/*" $ do
         route $ setExtension "html"
 
@@ -21,7 +39,7 @@ main = hakyll $ do
             let (body', md) = handleWikiLinks (itemBody body)
             let metadataCtxs =
                  map (\(k,v) -> constField k v) (M.toList md)
-            let ctx = (mconcat metadataCtxs) `mappend` postCtx
+            let ctx = (mconcat metadataCtxs) `mappend` (postCtxWithTags tags)
             do
                 renderPandoc (itemSetBody body' body)
                 >>= loadAndApplyTemplate "templates/post.html"    ctx
@@ -50,3 +68,6 @@ postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+postCtxWithTags :: Tags -> Context String
+postCtxWithTags tags = tagsField "tags" tags `mappend` postCtx
